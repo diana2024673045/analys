030f0e033598a888be06dd78f831f5d783b2b025
diana2024673045 <vakulinskaya.d@gmail.com>
2025-05-24 16:04:37 +0900

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+mbti_types = [
+    "INTJ", "INTP", "ENTJ", "ENTP",
+    "INFJ", "INFP", "ENFJ", "ENFP",
+    "ISTJ", "ISFJ", "ESTJ", "ESFJ",
+    "ISTP", "ISFP", "ESTP", "ESFP"
+]
+
+zodiac_signs = [
+    "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
+    "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"
+]
+
+animal_signs = [
+    "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+    "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
+]
+
+zodiac_signs_kr = [
+    "양자리", "황소자리", "쌍둥이자리", "게자리", "사자자리", "처녀자리",
+    "천칭자리", "전갈자리", "사수자리", "염소자리", "물병자리", "물고기자리"
+]
+
+animal_signs_kr = [
+    "쥐", "소", "호랑이", "토끼", "용", "뱀",
+    "말", "양", "원숭이", "닭", "개", "돼지"
+]
